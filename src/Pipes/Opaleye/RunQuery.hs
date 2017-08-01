581053ac602eda02cc2a44ed31b9b8dbd4119674
Bas van Dijk <v.dijk.bas@gmail.com>
2017-08-02 01:01:44 +0200

    Fix warnings

--- src/Pipes/Opaleye/RunQuery.hs
+++ src/Pipes/Opaleye/RunQuery.hs
@@ -2,19 +2,15 @@
 
 module Pipes.Opaleye.RunQuery ( query ) where
 
-import Control.Exception (handle, throwTo, fromException, SomeException, AsyncException(ThreadKilled))
 import Control.Monad.Catch (MonadMask)
 import Control.Monad.IO.Class (MonadIO, liftIO)
-import Opaleye (QueryRunner, Query, runQueryFold)
-import Opaleye.RunQuery (Cursor, declareCursor, closeCursor, foldForward)
+import Opaleye (QueryRunner, Query)
+import Opaleye.RunQuery (declareCursor, closeCursor, foldForward)
 import Data.Profunctor.Product.Default (Default)
 import Database.PostgreSQL.Simple (Connection)
 import Pipes (ListT(Select), yield)
 import Pipes.Safe (SafeT, bracket)
-import Control.Concurrent (ThreadId, forkIOWithUnmask, killThread, myThreadId)
-import Control.Concurrent.MVar (MVar, newEmptyMVar, takeMVar, putMVar)
 import Data.Function (fix)
-import Data.Foldable (for_)
 
 query :: forall columns haskells m
        . ( Default QueryRunner columns haskells
@@ -25,8 +21,8 @@ query :: forall columns haskells m
       -> Int -- ^ chunk size
       -> Query columns
       -> ListT (SafeT m) haskells
-query conn chunkSize query = Select $
-    bracket (liftIO $ declareCursor conn query)
+query conn chunkSize q = Select $
+    bracket (liftIO $ declareCursor conn q)
             (liftIO . closeCursor) $ \cursor ->
       fix $ \loop -> do
         r <- liftIO $ foldForward cursor chunkSize
